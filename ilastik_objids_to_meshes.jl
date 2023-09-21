47828a6f5b0bd41a6bdd81fd60cd1f41d57d7342
spelufo <santiagopelufo@gmail.com>
2023-09-21 18:31:09 -0300

    Add top_pipeline.jl

---
+++ ilastik_objids_to_meshes.jl
@@ -0,0 +1,42 @@
+using LinearAlgebra, GeometryBasics, MarchingCubes, GLMakie, FileIO, MeshIO, HDF5, Statistics, Graphs, SimpleWeightedGraphs
+
+
+mesh_and_save_hole!(M::Array{UInt32, 3}, i::UInt32, pos::Point3f, filename::String) = begin
+  divs = 8
+  samples = Float32(divs^3)
+  sx, sy, sz = div.(size(M), divs)
+  vol = zeros(Float32, (sx, sy, sz))
+  for iz = 1:sz, iy = 1:sy, ix = 1:sx
+    val = 0f0
+    for kz = 1:divs, ky = 1:divs, kx = 1:divs
+      inobj = M[divs*(ix-1)+kx, divs*(iy-1)+ky, divs*(iz-1)+kz] == i
+      val += Float32(inobj) / samples
+    end
+    vol[ix, iy, iz] = val - 0.5f0
+  end
+  mc = MC(vol)
+  # mc = MC(Float32(0.5) .- Float32.(M .== i))
+  march(mc)
+
+  println("Meshing ($(length(mc.vertices)) vertices)...")
+  if length(mc.vertices) < 3
+    println("not enough vertices")
+  else
+    msh = MarchingCubes.makemesh(GeometryBasics, mc)
+    msh.position .*= Float32(divs)
+    msh.position .+= pos
+    save(filename, msh)
+  end
+  nothing
+end
+
+hole_ids_to_meshes(hole_ids_file::String, file_prefix::String, pos::Point3f) = begin
+  f = h5open(hole_ids_file)
+  M = f["exported_data"][1, :, :, :, 1]
+  n = maximum(M)
+  close(f)
+  for id = 1:n
+    println("Building mesh for hole $id / $n ... ")
+    mesh_and_save_hole!(M, UInt32(id), pos, "$(file_prefix)$(id).stl")
+  end
+end
---
+++ top_pipeline.jl
@@ -0,0 +1,78 @@
+const CELLS_DIR = "../data/full-scrolls/Scroll1.volpkg/volume_grids/20230205180739"
+const SEGDATA_DIR = "../data/segmentation"
+
+include("cell_tif_to_h5.jl")
+include("ilastik_objids_to_meshes.jl")
+
+@inline zpad(i::Int, ndigits::Int)::String =
+  lpad(i, ndigits, "0")
+
+cell_filename(cell_jy::Int, cell_jx::Int, cell_jz::Int) =
+  "cell_yxz_$(zpad(cell_jy, 3))_$(zpad(cell_jx, 3))_$(zpad(cell_jz, 3))"
+
+
+top_pipeline(cell_jy::Int, cell_jx::Int, cell_jz::Int) = begin
+  # 0. Setup.
+  cell_name = cell_filename(cell_jy, cell_jx, cell_jz)
+  cell_dir = "$SEGDATA_DIR/$cell_name"
+  isdir(cell_dir) || mkdir(cell_dir)
+
+  # 1. Download from volume_grids.   `*.tif`                  ( 250M)
+  # I already did this manually, using download_grid_cells_range. TODO.
+
+
+  # 2. Convert to h5 for ilastik.    `*.h5`                   ( 250M) (  20s)
+  raw_h5_file = "$cell_dir/$cell_name.h5"
+  @time if !isfile(raw_h5_file)
+    println("Converting tif to h5...")
+    cell_to_h5("$CELLS_DIR/$cell_name.tif", raw_h5_file)
+  end
+
+  # 3. Ilastik pixel classification. `*_probabilities.h5`     (1000M) ( 180s)
+  probabilities_file = "$cell_dir/$(cell_name)_probabilities.h5"
+  @time if !isfile(probabilities_file)
+    println("Running ilastik pixel classification...")
+    run(pipeline(
+      `/opt/ilastik/run_ilastik.sh --project ./ilastik/Vesuvius.ilp
+      --headless --readonly
+      --export_source Probabilities
+      --output_filename_format $probabilities_file
+      $cell_dir/$cell_name.h5`,
+      stdout="/tmp/ilastik_pixel_classification.log",
+      stderr="/tmp/ilastik_pixel_classification.log"))
+    # TODO: Maybe try configuring the project with less features and see if it
+    # works just as well, so that it is faster. After having the whole pipeline.
+  end
+
+  # 4. Ilastik segmentation.         `*_hole_ids.h5`          ( 500M) (  28s)
+  hole_ids_file = "$cell_dir/$(cell_name)_hole_ids.h5"
+  @time if !isfile(hole_ids_file)
+    println("Running ilastik segmentation...")
+    run(pipeline(
+      `/opt/ilastik/run_ilastik.sh --project ./ilastik/Vesuvius_seg3.ilp
+      --headless --readonly
+      --export_source 'Object Identities'
+      --output_filename_format $hole_ids_file
+      --raw_data $cell_dir/$cell_name.h5
+      --prediction_maps $cell_dir/$(cell_name)_probabilities.h5`,
+      stdout="/tmp/ilastik_segmentation.log",
+      stderr="/tmp/ilastik_segmentation.log"))
+  end
+
+
+  # 5. Marching cubes.               `holes/*.stl`             ( 750M) ( 35s)
+  hole_dir = "$cell_dir/holes"
+  @time if !isdir(hole_dir)
+    println("Building hole meshes...")
+    mkdir(hole_dir)
+    cell_pos = Point3f(500f0 * (cell_jx-1), 500f0 * (cell_jy-1), 500f0 * (cell_jz-1))
+    hole_ids_to_meshes(hole_ids_file, "$hole_dir/$(cell_name)_hole_", cell_pos)
+  end
+
+  # 6. Cleanup intermediate files.
+  # rm(raw_h5_file)
+  # rm(probabilities_file)
+  # rm(hole_ids_file)
+
+  nothing
+end
